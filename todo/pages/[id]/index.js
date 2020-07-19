f6e4baaee47178c1c1972782d9944deed7bcf95b
Lina Ebeid <linaebe0@gmail.com>
2020-07-19 13:51:30 +0300

    note component by id

---
+++ todo/pages/[id]/index.js
@@ -0,0 +1,14 @@
+
+// render Note component by note info
+function Note() {
+  return (
+    <div>
+      <h1>title</h1>
+      <p style={{textAlign: 'center'}}>descreiption</p>
+    </div>
+  );
+}
+
+// get note by Id using getServerSideProps or getStaticProps, Think about it !
+
+export default Note;
